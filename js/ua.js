b7069c29d501fe5d3d4c39fd95ebdf9cf7e5289a
celerZhao <zhaowenzhen1986@126.com>
2018-07-13 14:57:02 +0800

    用户设备型号判断，跳转到不同的APP store

---
+++ js/ua.js
@@ -0,0 +1,19 @@
+/**
+ * Created by zhaowz on 2018/7/13.
+ */
+
+/**
+ * 判断浏览器所在机器操作系统版本
+ */
+function gotoDownload(){
+  var u = navigator.userAgent,version = '';
+  if (!!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/)) {
+    //ios
+    window.location.href="https://itunes.apple.com/cn/app/guang-hui-yun-you/id1071019636?l=zh&ls=8";
+  } else if (u.indexOf('Android') > -1 || u.indexOf('Adr') > -1  ) {
+    //android
+    window.location.href="https://app.brightoilonline.com/c2b/C2B_Phone.apk";
+  }else{
+	
+  }
+}

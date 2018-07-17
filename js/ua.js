d1d0bb598a956ea442aaf59965f149df37fa6f46
celerZhao <zhaowenzhen1986@126.com>
2018-07-17 18:11:16 +0800

    test scrollInToView

--- js/ua.js
+++ js/ua.js
@@ -3,7 +3,7 @@
  */
 
 /**
- * 判断浏览器所在机器操作系统版本
+ * 判断浏览器所在机器操作系统
  */
 function gotoDownload(){
   var u = navigator.userAgent,version = '';
@@ -13,7 +13,5 @@ function gotoDownload(){
   } else if (u.indexOf('Android') > -1 || u.indexOf('Adr') > -1  ) {
     //android
     window.location.href="https://app.brightoilonline.com/c2b/C2B_Phone.apk";
-  }else{
-	
   }
 }
